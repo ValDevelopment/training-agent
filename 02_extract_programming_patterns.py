5fc9125a6ad9ed0caeedd68d2fd0a1b350ef9a7a
Vladislav Fedorov <73140488+ValDevelopment@users.noreply.github.com>
2026-07-22 17:02:02 -0500

    weekly pattern data saved

--- 02_extract_programming_patterns.py
+++ 02_extract_programming_patterns.py
@@ -17,4 +17,6 @@ weekly_pattern = (df.groupby(["exercise", "week_num"])
                     .reset_index()
                     .sort_values(["exercise", "week_num"]))
 
+weekly_pattern.to_csv("weekly_programming_patterns.csv", index=False)
 print(weekly_pattern.to_string(index=False))
+
---
+++ 03_programming_patterns.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+weekly_pattern = pd.read_csv("weekly_programming_patterns.csv")
+
+print(weekly_pattern.head())
